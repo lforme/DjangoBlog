0f9b1f1dfaa07224e93f692ce404bc702495ba86
木瓜 <lforme@163.com>
2019-03-27 11:45:15 +0800

    修改文件位置

--- blogproject/blogproject/settings.py
+++ blogproject/blogproject/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6(_krr+_982plkiv(vjg791gx=@%a(r#l#=y(0^y!$d7aqvln_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.gaygaybar.cn']
 
